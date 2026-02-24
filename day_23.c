ada10b7c027b8982ca4f27e1dccd19aef5d98ed8
Ravi Choudhary <rc9999488@gmail.com>
2026-02-24 21:18:42 +0530

    Create day_23.c

---
+++ day_23.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create list
+struct Node* createList(int n) {
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    for(int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if(head == NULL) {
+            head = newNode;
+            temp = newNode;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+    return head;
+}
+
+// Function to merge two sorted lists
+struct Node* mergeLists(struct Node* l1, struct Node* l2) {
+
+    if(l1 == NULL) return l2;
+    if(l2 == NULL) return l1;
+
+    struct Node *result = NULL;
+
+    if(l1->data <= l2->data) {
+        result = l1;
+        result->next = mergeLists(l1->next, l2);
+    } else {
+        result = l2;
+        result->next = mergeLists(l1, l2->next);
+    }
+
+    return result;
+}
+
+int main() {
+    int n, m;
+
+    scanf("%d", &n);
+    struct Node* list1 = createList(n);
+
+    scanf("%d", &m);
+    struct Node* list2 = createList(m);
+
+    struct Node* merged = mergeLists(list1, list2);
+
+    // Print merged list
+    while(merged != NULL) {
+        printf("%d ", merged->data);
+        merged = merged->next;
+    }
+
+    return 0;
+}
