9e3216d26058712ba449c0b670520526247d884d
Ravi Choudhary <rc9999488@gmail.com>
2026-03-18 23:01:21 +0530

    Create day_46.c

---
+++ day_46.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    if (n == 0 || arr[0] == -1)
+        return 0;
+
+    struct Node* root = createNode(arr[0]);
+
+    struct Node* queue[n];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    int i = 1;
+
+    // Build Tree
+    while (i < n) {
+        struct Node* current = queue[front++];
+
+        if (arr[i] != -1) {
+            current->left = createNode(arr[i]);
+            queue[rear++] = current->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            current->right = createNode(arr[i]);
+            queue[rear++] = current->right;
+        }
+        i++;
+    }
+
+    // Level Order Traversal
+    front = 0;
+
+    while (front < rear) {
+        struct Node* temp = queue[front++];
+        printf("%d ", temp->data);
+
+        if (temp->left)
+            queue[rear++] = temp->left;
+
+        if (temp->right)
+            queue[rear++] = temp->right;
+    }
+
+    return 0;
+}
