3ad227fe413e78808c8820ce8ae9a9663ed35bf9
Ravi Choudhary <rc9999488@gmail.com>
2026-04-06 10:34:34 +0530

    Create day_64.c

---
+++ day_64.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Graph {
+    int V;
+    struct Node** adj;
+};
+
+struct Node* createNode(int v) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = v;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct Graph* createGraph(int V) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->V = V;
+
+    graph->adj = (struct Node**)malloc(V * sizeof(struct Node*));
+
+    for (int i = 0; i < V; i++)
+        graph->adj[i] = NULL;
+
+    return graph;
+}
+
+// Add edge (undirected)
+void addEdge(struct Graph* graph, int u, int v) {
+    struct Node* newNode = createNode(v);
+    newNode->next = graph->adj[u];
+    graph->adj[u] = newNode;
+
+    newNode = createNode(u);
+    newNode->next = graph->adj[v];
+    graph->adj[v] = newNode;
+}
+
+// BFS function
+void BFS(struct Graph* graph, int start) {
+    int visited[graph->V];
+    for (int i = 0; i < graph->V; i++)
+        visited[i] = 0;
+
+    int queue[MAX];
+    int front = 0, rear = 0;
+
+    visited[start] = 1;
+    queue[rear++] = start;
+
+    while (front < rear) {
+        int v = queue[front++];
+        printf("%d ", v);
+
+        struct Node* temp = graph->adj[v];
+        while (temp) {
+            if (!visited[temp->data]) {
+                visited[temp->data] = 1;
+                queue[rear++] = temp->data;
+            }
+            temp = temp->next;
+        }
+    }
+}
+
+int main() {
+    int n, m;
+    scanf("%d", &n);
+    scanf("%d", &m);
+
+    struct Graph* graph = createGraph(n);
+
+    int u, v;
+    for (int i = 0; i < m; i++) {
+        scanf("%d %d", &u, &v);
+        addEdge(graph, u, v);
+    }
+
+    int s;
+    scanf("%d", &s);
+
+    BFS(graph, s);
+
+    return 0;
+}
