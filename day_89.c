8d13b05b27ccbce8d755dd23570c22492a3b87cc
Ravi Choudhary <rc9999488@gmail.com>
2026-04-30 12:52:16 +0530

    Create day_89.c

---
+++ day_89.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// Check if allocation possible
+int canAllocate(int arr[], int n, int m, int maxPages) {
+    int students = 1;
+    int pages = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxPages)
+            return 0;
+
+        if (pages + arr[i] <= maxPages) {
+            pages += arr[i];
+        } else {
+            students++;
+            pages = arr[i];
+        }
+
+        if (students > m)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    int n, m;
+    scanf("%d %d", &n, &m);
+
+    int arr[n];
+    int sum = 0, maxVal = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+        if (arr[i] > maxVal)
+            maxVal = arr[i];
+    }
+
+    int low = maxVal, high = sum;
+    int ans = sum;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (canAllocate(arr, n, m, mid)) {
+            ans = mid;
+            high = mid - 1;  // minimize
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    printf("%d", ans);
+
+    return 0;
+}
